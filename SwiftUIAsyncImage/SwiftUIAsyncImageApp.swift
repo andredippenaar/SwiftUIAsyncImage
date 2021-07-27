57f3a57c3f240fade27c5977f775e6488433e36f
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-07-27 13:47:55 +0200

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Andre Dippenaar on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
